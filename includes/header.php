4ee78cb730e65bd37613f863701aee606a049b21
clockworkaiesh <33953822+clockworkaiesh@users.noreply.github.com>
2020-01-20 13:45:30 +0500

    Links updated
    All links in the menu and overall updated

---
+++ includes/header.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Shirkat-ul-Ras - Travels n Tours">
+    <meta name="Digital Eggheads" content="Shirkat-ul-Ras">
+    <title>Shirkat-ul-Ras</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="./img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="./img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="./img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="./img/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Gochi+Hand|Montserrat:300,400,700" rel="stylesheet">
+
+    <!-- COMMON CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="./css/vendors.css" rel="stylesheet">
+    
+    <!-- FONT AWESOME CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CUSTOM CSS -->
+    <link href="./css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/skr-custom.css">
+    
+</head>
+<body>
+
+    <div id="preloader">
+        <div class="sk-spinner sk-spinner-wave">
+            <div class="sk-rect1"></div>
+            <div class="sk-rect2"></div>
+            <div class="sk-rect3"></div>
+            <div class="sk-rect4"></div>
+            <div class="sk-rect5"></div>
+        </div>
+    </div>
+    <!-- End Preload -->
+
+    <div class="layer"></div>
+    <!-- Mobile menu overlay mask -->
+
+    <!-- Header================================================== -->
+    <header>
+        <div id="top_line">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <ul id="top_links_left">
+                            <li>
+                                <i class="icon-phone"></i><strong><a href="tel:92 301 8587645">+92 301 8587645</a></strong>
+                            </li>
+                            <li>
+                                <i class="icon-email"></i><strong><a href="mailto:info@shirkatulras.com">info@shirkatulras.com</a></strong>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div class="col-6">
+                        <ul id="top_links">
+                            <li><a href="#youtube_link" id="youtube_link">Youtube</a></li>
+                            <li><a href="#facebook_link" id="facebook_link">Facebook</a></li>
+                            <li><a href="#instagram_link" id="instagram_link">Instagram</a></li>
+
+                        </ul>
+                    </div>
+                </div><!-- End row -->
+            </div><!-- End container-->
+        </div><!-- End top line-->
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <div id="logo_home">
+                        <img src="img/Branding/logo.png" alt="City tours" data-retina="true">
+                        <h1></h1>
+                    </div>
+                </div>
+                <nav class="col-9">
+                    <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="javascript:void(0);"><span>Menu mobile</span></a>
+                    <div class="main-menu">
+                        <div id="header_menu">
+                            <img src="img/Branding/logo.png" alt="City tours" data-retina="true">
+                        </div>
+                        <a href="#" class="open_close" id="close_in"><i class="icon_set_1_icon-77"></i></a>
+                        <ul>
+                            <li class="submenu">
+                                <a href="index.html" class="show-submenu">Home</a>
+                            </li>
+                            <li class="submenu">
+                                <a href="about.html" class="show-submenu">About Us<i class="icon-down-open-mini"></i></a>
+                                <ul>
+                                    <li><a href="all_tours_list.html">Who We Are?</a></li>
+                                    <li><a href="all_tours_grid.html">Our Vision & Mission</a></li>
+                                    <li><a href="team.html">Our Team</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:void(0);" class="show-submenu">Services <i class="icon-down-open-mini"></i></a><ul>
+                                    <li><a href="all_hotels_list.html">Hotel Accomodations</a></li>
+                                    <li><a href="all_hotels_grid.html">Visa and Ticketing facilitations</a></li>
+                                    <li><a href="all_hotels_map_listing.html">Religious/Tour Guide/Local Guide</a></li>
+                                    <li><a href="single_hotel.html">Food</a></li>
+                                    <li><a href="single_hotel_datepicker_adv.html">Transport Service</a></li>
+                                    <li><a href="single_hotel_datepicker_v2.html">Medical First Aid </a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:void(0);" class="show-submenu">Destinations <i class="icon-down-open-mini"></i></a>
+                                <ul>
+                                    <li class="third-level"><a href="javascript:void(0);">Iraq</a>
+                                        <ul>
+                                            <li><a href="single_tour_fixed_sidebar.html">Karbala</a></li>
+                                            <li><a href="single_tour_fixed_sidebar.html">Najaf</a></li>
+                                            <li><a href="single_tour_with_gallery_fixed_sidebar.html">Kazmain<</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="third-level"><a href="javascript:void(0);">Iran</a>
+                                        <ul>
+                                            <li><a href="single_tour_fixed_sidebar.html">Mashhad</a></li>
+                                            <li><a href="single_tour_fixed_sidebar.html">Qom</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="third-level"><a href="javascript:void(0);">Syria</a>
+                                        <ul>
+                                            <li><a href="single_tour_fixed_sidebar.html">Damascus</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="third-level"><a href="javascript:void(0);">Kingdom of Saudi Arabia</a>
+                                        <ul>
+                                            <li><a href="single_tour_fixed_sidebar.html">Makkah</a></li>
+                                            <li><a href="single_tour_fixed_sidebar.html">Madina</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="hotels.html" class="show-submenu">Hotels</a>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:void(0);" class="show-submenu">Trip Planner</a>
+                            </li>
+                            <li class="submenu">
+                                <a href="partners.html" class="show-submenu">Partners</a>
+                            </li>
+                            <li class="submenu">
+                                <a href="contact.html" class="show-submenu">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div><!-- End main-menu -->
+ <!--                    <ul id="top_tools">
+                        <li>
+                            <a href="javascript:void(0);" class="search-overlay-menu-btn"><i class="icon_search"></i></a>
+                        </li>
+                    </ul> -->
+                </nav>
+            </div>
+        </div><!-- container -->
+    </header><!-- End Header -->
