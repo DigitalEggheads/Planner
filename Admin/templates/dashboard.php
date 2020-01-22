0ccb3f0e68b3620a4cc4c730a565aaf244d99137
clockworkaiesh <33953822+clockworkaiesh@users.noreply.github.com>
2020-01-22 08:28:39 +0500

    add hotel & manage hotel
    manage and add hote dahsboard pages added

--- Admin/templates/dashboard.php
+++ Admin/templates/dashboard.php
@@ -79,7 +79,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="dashboard.php" class="brand-link">
       <img src="dist/img/Branding/logo.png" alt="Shirkat-ul-Ras Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Shirkat-ul-Ras</span>
