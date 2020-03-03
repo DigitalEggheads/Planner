f7ff8cfe1072fc8b3f934c80eb4d44a34e34ec41
clockworkaiesh <33953822+clockworkaiesh@users.noreply.github.com>
2020-03-03 13:42:02 +0500

    crazy trip planner changes

--- Footer.php
+++ Footer.php
@@ -169,6 +169,18 @@
         }
     });
     </script>
+    <script>
+        // All this jquery is just used for presentation. Not required at all for the radio buttons to work.
+$( document ).ready(function(){
+//   Hide the border by commenting out the variable below
+    var $on = 'section';
+    $($on).css({
+      'background':'none',
+      'border':'none',
+      'box-shadow':'none'
+    });
+}); 
+    </script>
 
 
     
--- includes/header.php
+++ includes/header.php
@@ -29,7 +29,6 @@
     <!-- CUSTOM CSS -->
     <link href="./css/custom.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/skr-custom.css">
-    
 </head>
 <body>
 
