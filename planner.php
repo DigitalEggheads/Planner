b24f72a8dd326edca1f643885df78f13532f9bff
DigitalEggheads <digitaleggheads1@gmail.com>
2020-03-04 10:51:44 +0500

    Some Changes

--- planner.php
+++ planner.php
@@ -473,7 +473,7 @@
                                                         <div class="country-form">
                                                             <div class="form__options">
                                                                 <p class="form__answer"> 
-                                                                    <input type="checkbox" name="match" id="match_1" value="country-iraq" checked required> 
+                                                                    <input type="checkbox" name="match" id="match_1" value="country-iraq" required> 
                                                                     <label for="match_1" class="iraq-label">
                                                                         <img class="checked-tick  mx-auto" src="img/Planner/hotel-checked.png" alt="">
                                                                         
@@ -684,4 +684,165 @@
 
 
 
-<?php include_once "./Footer.php"; ?>
+
+<footer class="revealed">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                        <a href="https://shirkatulras.com/">
+                            <img class="img-responsive mw-100" src="img/Branding/FooterLogo.png" alt="Footer Logo">
+                        </a>
+                <br/>
+                <p class="Footer-About p-2 pt-4">Shirkat ul Ras is reliable name for religious tourism. Our professional team is always ready to guide the zaireen on their trips to Iran, Iraq, Syria and Saudia Arabia. We are extending the religious tourism to make traveling easier to dedicated location.</p>
+
+                <div id="social_footer" class="mb-2 pb-2">
+                    <ul>
+                        <li><a href="https://www.facebook.com/ShirkatulRas/"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#"><i class="icon-instagram"></i></a></li>
+                        <li><a href="#"><i class="icon-youtube-play"></i></a></li>
+                    </ul>
+                </div>
+
+                
+                    
+                <div class="alsahlah-affiliation p-0 m-0">
+                    <p class="text-light pt-4 mt-4 pl-2 pr-2" style="margin-bottom: 0px;">For Standard Packages</p>
+                    <a href="https://alsahlah.com/"  rel="nofollow" target="_blank">
+                        <img class="img-responsive mw-100" src="img/Branding/AlSahlah-Logo.png" alt="Al-Sahlah-Logo">
+                    </a>
+                </div>
+
+                
+
+                
+                
+            </div>
+            
+            <div class="col-md-3">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="hotels.php">Hotels</a></li>
+                    <li><a href="#">Trip Planner</a></li>
+                </ul>
+                <h3>Destinations</h3>
+                <ul>
+                    <li><a href="iraq.php">Iraq</a></li>
+                    <li><a href="iran.php">Iran</a></li>
+                    <li><a href="syria.php">Syria</a></li>
+                    <li><a href="ksa.php">Kingdom Of Saudi Arabia</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h3>Need help?</h3>
+                <p id="address" class="pt-0">
+                Suite #1, Mezzanine Floor, Hanif Mansion, Opp. Nisthar Park, Soldier Bazar No.3, Karachi-Pakistan.
+                </p>
+                <a href="tel:+923018587645" id="phone">+92 301 8587645</a>
+                <a href="mailto:info@shirkatulras.com" id="email_footer">info@shirkatulras.com</a>
+            </div>
+        </div><!-- End row -->
+        <div class="row text-center">
+            <div class="col-md-12 footer-copyrights">
+                <p>© Copyright 2020 - <a href="https://shirkatulras.com/" style="color: #d3ba37;">Shirkat-ul-Ras</a>. Developed by: <a href="https://digitaleggheads.com/" target="_blank" style="color: #d3ba37;">Digital Eggheads</a></p>
+            </div>
+        </div><!-- End row -->
+    </div><!-- End container -->
+</footer><!-- End footer -->
+
+<div id="toTop"></div><!-- Back to top button -->
+
+<!-- Search Menu -->
+<div class="search-overlay-menu">
+  <span class="search-overlay-close"><i class="icon_set_1_icon-77"></i></span>
+  <form role="search" id="searchform" method="get">
+     <input value="" name="q" type="search" placeholder="Search..." />
+     <button type="submit"><i class="icon_set_1_icon-78"></i>
+     </button>
+ </form>
+</div><!-- End Search Menu -->
+
+
+
+<!-- Date and time pickers -->
+    <script src="js/jquery.sliderPro.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function($) {
+            $('#Img_carousel').sliderPro({
+                width: 960,
+                height: 500,
+                fade: true,
+                arrows: true,
+                buttons: false,
+                fullScreen: false,
+                smallSize: 500,
+                startSlide: 0,
+                mediumSize: 1000,
+                largeSize: 3000,
+                thumbnailArrows: true,
+                autoplay: false
+            });
+        });
+    </script>
+
+
+    <!-- Date and time pickers -->
+    <script src="js/datepicker_advanced.js"></script>
+
+    
+    
+    <!--Review modal validation -->
+    <script src="assets/validate.js"></script>
+
+<!-- Common scripts -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/common_scripts_min.js"></script>
+<script src="js/functions.js"></script>
+
+<!-- Specific scripts -->
+    <script src="js/jquery.validate.js"></script>
+    <script src="js/plannerjs/script.js"></script>
+   
+
+<!-- Video header scripts -->
+<script src="js/modernizr.js"></script>
+<script src="js/video_header.js"></script>
+
+
+
+    
+     
+   
+
+    
+<!-- Step 4 submit form success message script -->
+<script> 
+
+$('.success-popup').hide();
+$( "#tips" ).click(function(e) {
+    e.preventDefault();
+  $('.success-popup').show();
+  $('.success-popout').hide();
+});
+
+
+
+</script>
+
+
+
+    
+
+</body>
+</html>
+
+
+
+
+
+ 
+
+    
+    
+    
+    
