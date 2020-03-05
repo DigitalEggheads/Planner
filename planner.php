5170ef6fd7ed8b3c62f72481aa46aea1aa4aab0f
clockworkaiesh <33953822+clockworkaiesh@users.noreply.github.com>
2020-03-05 16:24:16 +0500

    re-added last two buttons on fourth step of trip planner form

--- planner.php
+++ planner.php
@@ -794,16 +794,16 @@
                              <fieldset>
                                 <div class="text-center p-4 m-4">
                                 <h3 class="pb-4 pt-2 success-popout">Confirm Submission</h3>
-                                <button type="button" value="submit" id="tips" class="btn btn-standard btn-block btn-form-submit mt-4 success-popout">Submit</button>
+                                <!-- <button type="button" value="submit" id="tips" class="btn btn-standard btn-block btn-form-submit mt-4 success-popout">Submit</button> -->
                                   <img src="img/Planner/success.png" class="mx-auto success-popup" width="80px" alt="">
                                   <p class="pt-4 pb-4 success-popup">Success! Details have been submitted. Check your email!</p>
                                 </div>
                                 
                                 <br/>
-                                <!-- <div class="form-wizard-buttons">
+                                <div class="form-wizard-buttons">
                                     <button type="button" class="btn btn-previous">Previous</button>
                                     <button type="submit" id="tips" class="btn btn-submit">Submit</button>
-                                </div> -->
+                                </div>
                             </fieldset>
 
                             <!-- Form Step 4 -->
